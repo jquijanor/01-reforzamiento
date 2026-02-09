d3273efb2e3b9c6a18ee092487d027c366369db3
jquijanor <julioquijanor@gmail.com>
2026-02-09 14:31:48 -0500

    Primer Commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<div> 
+<h1>Hola Mundo...<h1>
+</div>
+`
+
