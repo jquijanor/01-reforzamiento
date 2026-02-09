7e4053efc376c01f195af6ef3e1e5aa5ef485836
jquijanor <julioquijanor@gmail.com>
2026-02-09 16:09:59 -0500

    Ejercicio 2

---
+++ src/bases/02-template-string.ts
@@ -0,0 +1,7 @@
+const firstName:string =  'Julio';
+const lastName =  "O'Neal";
+const fullName = `${firstName} ${lastName}`
+
+console.log(fullName);
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
+import './bases/02-template-string'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div> 
