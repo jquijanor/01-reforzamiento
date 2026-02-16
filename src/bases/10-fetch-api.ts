6aa19f18b72af0d055d12b6acdc2b58f4b0fae82
jquijanor <julioquijanor@gmail.com>
2026-02-16 16:51:06 -0500

    ImageElemento 01

--- src/bases/10-fetch-api.ts
+++ src/bases/10-fetch-api.ts
@@ -4,3 +4,11 @@ const myRequest = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}
 //     console.log(Response);
 //     Response.json( ).then ((data) => { console.log(data);});
 // }).catch ( err => {console.error(err);})
+myRequest.then( (Response) => Response.json()).then((data) => {
+    const imageUrl = data.data.images.original.url;
+    console.log(imageUrl);
+    const imgElement = document.createElement('img');
+    imgElement.src = imageUrl;
+    document.body.append(imgElement);
+
+}).catch(( err) => { });
